f80943c35a611b735e0b1634996d1cbe8f7a3fd4
Vaclav Synacek <evasek@gmail.com>
2021-03-17 18:25:38 +0100

    minimal web server working

--- player.lisp
+++ player.lisp
@@ -5,13 +5,44 @@
   (:use :cl :usocket :uiop))
 (in-package :player)
 
+(defun h200 ()
+    (format t "HTTP/1.0 200 OK~%~%"))
+
+(defun h400 ()
+    (format t "HTTP/1.0 400 Bad Request~%~%"))
+
+(defun index ()
+  (h200)
+  (format t "<html>
+<head><title>Minimal Player</title></head>
+<body>
+  <h1>Minimal Player</h1>
+  <ul>
+    <li><form action=\"\\\" method=\"post\">
+          <input type=\"submit\" value=\"Play\" />
+        </form> <span>bla</span></li>
+    <li>fuj</li>
+  </ul>
+</body></html>"))
+
+(index)
+
+(defun play ()
+  (h200))
+
 (defun serve (port)
-  (usocket:with-socket-listener (socket "127.0.0.1" port :reuse-address t)
+  (usocket:with-socket-listener (socket "0.0.0.0" port :reuse-address t)
     (loop 
        (usocket:with-server-socket (connection (usocket:socket-accept socket))
         (with-open-stream (stream (usocket:socket-stream connection))
-           (let* ((first-line (read-line stream)))
-                  ;(*standard-output* stream))       
-              (princ first-line)))))))
+           (let* ((first-line (read-line stream))
+                  (*standard-output* stream))       
+             (cond
+               ((string= "GET / " (subseq first-line 0 6))  (index))
+               ((string= "POST / " (subseq first-line 0 7)) (play))
+               (t (h400)))))))))
 
-(serve 9900)
+
+;(princ first-line)))))))
+
+(serve 9904)
