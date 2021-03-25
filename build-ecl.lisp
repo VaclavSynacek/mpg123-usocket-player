29dc4ab245ec8895571f6e2dcad2b6f75b22afa0
Vaclav Synacek <evasek@gmail.com>
2021-03-25 20:01:03 +0100

    now should build in ecl 20.1+++

--- build-ecl.lisp
+++ build-ecl.lisp
@@ -4,37 +4,12 @@
 
 (asdf:operate 'asdf:load-op 'minimal-player)
 
-(asdf:make-build :usocket
-                 :type :static-library
-                 :move-here #P"./"
-                 :init-name "init_usocket")
-
-(asdf:make-build :split-sequence
-                 :type :static-library
-                 :move-here #P"./"
-                 :init-name "init_split_sequence")
-
-(asdf:make-build :minimal-player
-                 :type :static-library
-                 :move-here #P"./"
-                 :init-name "init_player")
-
-(c:build-program "minimal-player-ecl"
-                 :lisp-files '("split-sequence.a" "usocket.a" "minimal-player.a")
-                 :prologue-code '(progn
-                                   (require 'sb-bsd-sockets)
-                                   (require 'asdf))
-                 :epilogue-code '(player:main))
-
-#|
-
 (asdf:make-build
   :minimal-player
   :type :program
   :move-here #P"./"
   :monolithic t
   :prologue-code '(progn
-                    (require 'sb-bsd-sockets)
-                    (require 'asdf))
+                    (require 'asdf)
+                    (require 'sb-bsd-sockets))
   :epilogue-code '(player:main))
-|#
